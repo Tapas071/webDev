60dfa802bbf6319a5daee7caf12f9909bad96fd3
Tapas Mondal <tapas710mondal@gmail.com>
2025-01-28 18:56:11 +0530

    adding js interview questions

--- interview.js
+++ interview.js
@@ -79,6 +79,7 @@ console.log(memoizeSquare());
 
  */
 
+/*
 // currying: series of function-> when one function take one argument and return  a function which takes argument  and the chain may proceed futher. This technique is called curring
 
 function sum ( a ){
@@ -111,3 +112,187 @@ function curry( func){
 }
 
 
+
+*/
+
+
+
+// --------------------------------------- who check whether one property one attribute is present inside one object------------------
+ /*
+let person = {
+    name : "Alice",
+    age : 30
+}
+
+// to add a property you can do
+person.class = 12;
+
+person[hobby] ="chess";
+
+// to delete a property you can do
+
+delete person.name
+
+// to check wheather a property is present or not
+
+console.log(age in person); // log : true  as it is present
+console.log( "name" in person) // it will log false
+
+// the same thing can be done using hasOwnProperty method
+console.log(person.hasOwnProperty("age")); // it will console true
+
+// using undefined check
+console.log(person.name !== undefined);
+
+// using optional chaining
+console.log(person?.name)
+
+*/
+
+/*
+// --- this keyword related questions ----
+
+function showThis() {
+    console.log(this);
+}
+
+showThis();
+
+// it will give you the reference of the global bject i.e window global object
+
+const obj = {
+    name: "Alice",
+    showThis: function () {
+        console.log(this.name);
+    },
+};
+
+obj.showThis();
+
+// Answer will be Alice
+
+const obj2 = {
+    name: "Bob",
+    showThis: function () {
+        console.log(this.name);
+    },
+};
+
+const externalFunc = obj2.showThis;
+externalFunc();
+
+// it will give you undefine , because the showthis  attributes gives you a function and that function that this keyboard , and this keyboard will be reference to global, and inside the global name is not defined
+
+const obj3 = {
+    name: "Charlie",
+    showThis: () => {
+        console.log(this.name);
+    },
+};
+
+obj3.showThis();
+
+// here the object define is not done, so it is only excrating the functon and inside the function this keyword will indicate the global object and the name attribute in the global object is not defined. so it will give you undefined
+
+*/
+
+// ------------------------------
+
+/*
+// different types of loops in js
+const fruits = ["apple", "banana", "mango"];
+
+//  for loop
+for(let i=0; i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+
+// using for each method
+fruits.forEach((fruit,i)=>{
+    console.log(fruit);
+
+}
+)
+
+// using for of loop
+for( const fruit of fruits){
+    console.log(fruit)
+}
+
+// using for in loop
+
+for( const i in fruits){
+    console.log(fruits[i]);
+}
+
+// using map method 
+const allFruits = fruits.map((fruit,ind)=>{
+    console.log(fruit)
+})
+allFruits;
+
+// using while loop
+let i =0; 
+while(i < fruits.length){
+    console.log(fruits[i]);
+    i++;
+}
+
+*/
+
+// ------------------ array method in js-----------
+
+const fruits = ["apple", "banana", "orange"];
+
+// to add an element inside an array from the back
+fruits.push("mongo");
+
+// to add an element inside an array from the front
+fruits.unshift("jackFruit");
+
+// to remove an element from an array from the front
+fruits.shift();
+// to remove an element from an array from the back
+fruits.pop();
+
+// adding/removing at specific index
+
+fruits.splice(1,0,"kiwi");
+// console.log(fruits);
+
+const number = [1,2,3,4,5,6,7,8,9,10]
+// filter method returns a new array with all the elements that pass the test
+const evenNumber = number.filter((num)=>{
+    return num % 2==0;
+})
+// console.log(evenNumber)
+ // return the first elemtn that satisfies the condition
+ const firstEvenNumber = number.find((num)=>{
+    return num%2==0;
+ })
+ console.log(firstEvenNumber);
+ 
+ const people = [
+    { name : "alice", age:25},
+    { name : "Bob", age:30},
+    { name : "Charlie", age:35}
+ ]
+ const anyOver30 = people.some((person)=>{
+    return person.age>=30;
+ })
+ // it will return true as one value is greater than equal to person age.
+
+ const allOver20 = people.every((person)=>{
+    return person.age>=20;
+ })
+
+ // spreading arrays
+ const arr1 = [1,2,3];
+ const arr2 = [4,5,6];
+ const combined = [...arr1, ...arr2];
+ console.log(combined);
+ // spreading of string
+
+ const str = "hello";
+ const chars = [...str];
+ console.log(chars);
