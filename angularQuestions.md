15be56386dba830cb37d7f56a87d292574ac04f5
Tapas Mondal <tapas710mondal@gmail.com>
2025-02-05 00:11:09 +0530

    adding angular interview questions

---
+++ angularQuestions.md
@@ -0,0 +1,4 @@
+# Angular Interview questions
+
+1. What is Angular?
+- Angular is a javaScript based ui framework which helps to connect Model and with Views in the MVC arthitecture.
--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+// const loadText = document.querySelector('.loading-text')
+// const bg = document.querySelector('.bg')
 
-let load = 0
+// let load = 0
 
-let int = setInterval(blurring, 30)
+// let int = setInterval(blurring, 30)
 
-function blurring() {
-  load++
+// function blurring() {
+//   load++
 
-  if (load > 99) {
-    clearInterval(int)
-  }
+//   if (load > 99) {
+//     clearInterval(int)
+//   }
 
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
+//   loadText.innerText = `${load}%`
+//   loadText.style.opacity = scale(load, 0, 100, 1, 0)
+//   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+// }
 
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
+// // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+// const scale = (num, in_min, in_max, out_min, out_max) => {
+//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+// }
+
+
+const arr = [1,2,4,5,7];
+const modArr = arr.map((num)=>{
+  return num *2;
+})
+console.log(modArr);
+
+
+const oddEle = arr.filter((num)=>{
+  return num%2
+})
+console.log(oddEle);
+
+const sum = arr.reduce((num,acc)=>{
+  return num + acc;
+})
+console.log(sum);
